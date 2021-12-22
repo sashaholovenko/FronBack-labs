64d6265bb5e58202f6d2f2af64b36b20d9665d59
sashaholovenko <sasha155s49@gmail.com>
2021-12-22 22:19:57 +0200

    1-4 labs

---
+++ lab1/1.js
@@ -0,0 +1,87 @@
+function view1(){
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+
+
+}
+
+function view2(){
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+
+}
+
+function view3(){
+    document.getElementById("div3").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+
+}
+
+
+function view(){
+    document.getElementById("a1").style.display = "block";
+    document.getElementById("a2").style.display = "block";
+    document.getElementById("a3").style.display = "block";
+    document.getElementById("a4").style.display = "block";
+}
+
+function op1(){
+    document.getElementById("div11").style.display = "block";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+}
+
+function op2(){
+    document.getElementById("div12").style.display = "block";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+}
+
+function op3(){
+    document.getElementById("div13").style.display = "block";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div14").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+}
+
+function op4(){
+    document.getElementById("div14").style.display = "block";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div13").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+}
+
+
---
+++ lab1/README.md
@@ -0,0 +1,3 @@
+# back-frond-end-labs
+
+labs for the subject "Основи back-end і front-end розробок" 3rd year
